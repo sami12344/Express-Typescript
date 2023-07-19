126fd8e8af3efcbd64e64bc6aa1552759ce3b76e
sami12344 <freelancersami16203@gmail.com>
2023-07-19 20:47:42 +0600

    Serving Static Files

--- src/Serving Static Files/staticServer.ts
+++ src/Serving Static Files/staticServer.ts
@@ -2,12 +2,15 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+app.use(
+  express.static(`${__dirname}/public`, {
+    index: 'home.html',
+  })
+)
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hi')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-
